48afd932357a6e043387f2b7df992fe305dfc0c8
Matt Farstad <matthewwilliamfarstad@gmail.com>
2016-01-20 11:27:36 -0600

    Joey

--- src/lmtstfy/TechSupportModel.java
+++ src/lmtstfy/TechSupportModel.java
@@ -1,6 +1,7 @@
 package lmtstfy;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.URL;
 import java.net.URLEncoder;
@@ -13,36 +14,14 @@ public class TechSupportModel {
 
 	public static void main(String[]args){
 		try {
-			JSONDemo("https://www.youtube.com/watch?v=phAKccY6M9I");
+			JSONDemo("tree");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 	public static void JSONDemo(String addr) throws IOException{
-		// build a URL
-	    String s = "http://maps.google.com/maps/api/geocode/json?" +
-	                    "sensor=false&address=";
-	    s += URLEncoder.encode(addr, "UTF-8");
-	    URL url = new URL(s);
-
-	    // read from the URL
-	    Scanner scan = new Scanner(url.openStream());
-	    String str = new String();
-	    while (scan.hasNext())
-	        str += scan.nextLine();
-	    scan.close();
-
-	    // build a JSON object
-	    JSONObject obj = new JSONObject(str);
-	    if (! obj.getString("status").equals("OK"))
-	        return;
-
-	    // get the first result
-	    JSONObject res = obj.getJSONArray("results").getJSONObject(0);
-	    System.out.println(res.getString("formatted_address"));
-	    JSONObject loc =
-	        res.getJSONObject("geometry").getJSONObject("location");
-	    System.out.println("lat: " + loc.getDouble("lat") +
-	                        ", lng: " + loc.getDouble("lng"));
+			URL url = new URL("https://graph.facebook.com/search?q=java&type=post");
+			try (InputStream is = url.openStream()
+				JSONParser parser = new JSONParser();
 		}
 	}
