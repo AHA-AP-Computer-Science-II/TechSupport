c11c7634e47777b8232ff13d21af827f3a054347
Caleb Reiter <calreiter16@ahastars.org>
2016-01-26 10:54:17 -0600

    copy/paste code

--- src/lmtstfy/CalebTest.java
+++ src/lmtstfy/CalebTest.java
@@ -1,5 +1,21 @@
 package lmtstfy;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
 public class CalebTest {
 
+	public static void main(String[] args) throws Exception {
+
+        URL oracle = new URL("http://www.oracle.com/");
+        BufferedReader in = new BufferedReader(
+        new InputStreamReader(oracle.openStream()));
+
+        String inputLine;
+        while ((inputLine = in.readLine()) != null)
+            System.out.println(inputLine);
+        in.close();
+    }
+	
 }
