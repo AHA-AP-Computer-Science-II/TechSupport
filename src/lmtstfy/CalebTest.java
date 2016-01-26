69d7a9a70c56f3146d5b8e601d10ab744fb86d3d
Caleb Reiter <calreiter16@ahastars.org>
2016-01-26 14:38:57 -0600

    JAVAFX HAS A WEB BROWSER WHY ARE WE USING JSON

--- src/lmtstfy/CalebTest.java
+++ src/lmtstfy/CalebTest.java
@@ -1,11 +1,33 @@
 package lmtstfy;
 
-public class CalebTest {
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+
+public class CalebTest extends Application {
+
+	public static void main(String[] args) {
+		launch(args);
+	}
 	
-	public static void main(String[] args) throws Exception {
+	@Override
+	public void start(Stage stage) throws Exception {
+
+		stage.setTitle("Tech Support");
 		
+		Group root = new Group();
+		WebView browser = new WebView();
+		WebEngine engine = browser.getEngine();
+		engine.load("http://www.google.com/");
+		Scene scene = new Scene(browser);
 		
+		stage.setScene(scene);
 		
-    }
+		stage.show();
+		
+	}
 	
 }
