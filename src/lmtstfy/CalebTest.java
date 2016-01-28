06d77f4a402a13cbdaa65a2a0d3f5b4398fb7065
Caleb Reiter <calreiter16@ahastars.org>
2016-01-28 10:51:43 -0600

    changed url

--- src/lmtstfy/CalebTest.java
+++ src/lmtstfy/CalebTest.java
@@ -38,8 +38,7 @@ public class CalebTest extends Application {
 			problem = result.get();
 		}
 		
-		engine.load("http://www.google.com/webhp?#q=" + problem +" windows 7 my computer won't start"
-				+ "troubleshoot" + "&btnI=I");
+		engine.load("http://www.google.com/webhp?#q=" + problem + "windows 7 troubleshoot" + "&btnI=I");
 
 		Scene scene = new Scene(browser);
 		
